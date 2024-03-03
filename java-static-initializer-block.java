6bb6defa521252dce929186adc6d373968287903
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-03-04 00:41:00 +0600

    Create java-static-initializer-block.java

---
+++ java-static-initializer-block.java
@@ -0,0 +1,60 @@
+/*
+Java Static Initializer Block
+
+Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
+
+It's time to test your knowledge of Static initialization blocks. You can read about it here.
+
+You are given a class Solution with a main method. Complete the given code so that it outputs the 
+area of a parallelogram with breadth B and height H. You should read the variables from the standard input.
+
+If B≤0 or H ≤0, the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+Input Format
+
+There are two lines of input. The first line contains B: the breadth of the parallelogram. 
+The next line contains H: the height of the parallelogram.
+
+Constraints 
+−100≤B≤100 
+−100≤H≤100
+
+Output Format
+
+If both values are greater than zero, then the main method must output the area of the parallelogram. 
+Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+     
+     Scanner input = new Scanner(System.in);
+
+        int breadth = input.nextInt(); 
+        int height = input.nextInt(); 
+
+        if (-100<=breadth && breadth<=100 && -100<=height && height<=100) {
+
+            if (breadth>0 && height>0) {
+            
+                int mul = breadth*height;
+                System.out.println(mul);
+    
+            }
+            else if (breadth<=0 || height<=0) {
+    
+                System.out.println("java.lang.Exception: Breadth and height must be positive");
+    
+            }
+            
+        }
+        
+        
+    }
+}
